19c58e035638f90f09cc51cae8d932d29128bb98
edwin0129 <edwin.v.cuenca@gmail.com>
2016-02-01 18:16:57 -0500

    agrege una variable

--- prueba_1/src/prueba_1/aaaaaaaasdasdasd.java
+++ prueba_1/src/prueba_1/aaaaaaaasdasdasd.java
@@ -2,4 +2,23 @@ package prueba_1;
 
 public class aaaaaaaasdasdasd {
 
+	
+	private int medidaedwin;
+	
+	
+	
+	
+	
+	
+
+	public int getMedidaedwin() {
+		return medidaedwin;
+	}
+
+	public void setMedidaedwin(int medidaedwin) {
+		this.medidaedwin = medidaedwin;
+	}
+	
+	
+	
 }
